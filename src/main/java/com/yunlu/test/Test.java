048b86e296598c2e10ec443c0a09b4caaf94c426
verphen <verphen@163.com>
2015-04-02 14:59:15 +0800

    test

---
+++ src/main/java/com/yunlu/test/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @Date 2015年3月31日  上午10:34:39
+ * @author 张亚飞
+ * @email verphen@163.com
+ */
+
+package com.yunlu.test;
+
+public class Test {
+	public static void main(String[] args) {
+
+		String str = "YLstring";
+		System.err.println(str.indexOf("."));
+		System.err.println(str.substring(str.indexOf(".")+1));
+	}
+}
