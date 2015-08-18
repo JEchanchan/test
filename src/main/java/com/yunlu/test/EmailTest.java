8ef344345d2715d7c21f0dc3f3d5a5815e267d2f
effine <verphen@163.com>
2015-08-18 17:16:30 +0800

    test

--- src/main/java/com/yunlu/test/EmailTest.java
+++ src/main/java/com/yunlu/test/EmailTest.java
@@ -18,16 +18,19 @@ public class EmailTest {
 	}
 	
 	public static void sendYunluEmail(){
-		String subject = "面试邀请"; // 主题
+		String subject = "发邮件测试"; // 主题
 		HtmlEmail email = new HtmlEmail();
-		email.setHostName("124.192.148.36"); // 设置发信的smtp服务器
+//		email.setHostName("124.192.148.36"); // 设置发信的smtp服务器
+		email.setHostName("smtp.qq.com"); // 设置发信的smtp服务器
 //		email.setHostName("mail.yunlu.me");
 		email.setSubject(subject); // 设置邮件主题
-		email.setAuthentication("noreplay@mail.yunlu.me","Yunlu201*");// SMTP服务器认证,设置帐号、密码
+//		email.setAuthentication("noreplay@mail.yunlu.me","Yunlu201*");// SMTP服务器认证,设置帐号、密码
+		email.setAuthentication("waiter@yunlu.info","Yunlu201*");// SMTP服务器认证,设置帐号、密码
 		email.setCharset("utf-8"); // 设置邮件字符集
 		try {
-			email.addTo("noreplay@mail.yunlu.me", "effine"); // 设置收件人帐号和收件人
-			email.setFrom("noreplay@mail.yunlu.me","wu wang"); // 设置发信的邮件帐号和发信人
+			email.addTo("verphen@163.com", "effine"); // 设置收件人帐号和收件人
+//			email.setFrom("noreplay@mail.yunlu.me","wu wang"); // 设置发信的邮件帐号和发信人
+			email.setFrom("waiter@yunlu.info","发邮件人"); // 设置发信的邮件帐号和发信人
 			email.setHtmlMsg("邮件内容 email content"); // 设置邮件正文，此方法这里的样式可以显示出来
 			email.send();
 			System.err.println("邮件发送成功");
--- src/main/java/com/yunlu/test/Test.java
+++ src/main/java/com/yunlu/test/Test.java
@@ -8,9 +8,30 @@ package com.yunlu.test;
 
 public class Test {
 	public static void main(String[] args) {
-
-		String str = "YLstring";
-		System.err.println(str.indexOf("."));
-		System.err.println(str.substring(str.indexOf(".")+1));
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int zeroNum = 0;
+		int clearNum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] != 0){
+				clearNum ++;
+			}else{
+				zeroNum++;
+			}
+			
+			if (zeroNum == arr.length - 2) {
+				System.err.println("最后一个人的位置：" + i);
+				break;
+			}
+			
+			if (clearNum == 3) {
+				arr[i] = 0;
+				clearNum = 0;
+				zeroNum = 0;
+			}
+			
+			if(i == arr.length-1){
+				i = -1;
+			}
+		}
 	}
 }
